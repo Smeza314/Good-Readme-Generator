b8a64206d80f0913cb82de76390d8b7a0d759154
Smeza314 <sebastianmeza314@gmail.com>
2021-03-05 14:35:49 -0800

    added some content to the markdown

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require('inquirer')
 const fs = require('fs')
+const generateMarkdown = require('./utils/generateMarkdown')
 // github username input
 const gitHubQuestion = [
   {
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,25 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
 
+  # Table of Contents
+  * [Project Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Tests](#tests)
+  * [Contributors](#contributors)
+  * [License](#license)
+  * [Questions](#questions)
+
+  #Project Description
+  ${data.description}
+
+  #Installation
+  ${data.installation}
+
+  #Usage
+  ${data.usage}
 `;
 }
 
